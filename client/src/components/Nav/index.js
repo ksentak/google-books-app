084d3fd284f926390018cc41d06b0bca692e6aa6
KSentak <k.sentak@outlook.com>
2019-12-24 10:40:57 -0500

    Modified Navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
-
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg text-light">
-            <div className="container">
-                <Link className="navbar-brand" to="/">Google Books</Link>
+
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div className="navbar-brand text-white">Google Books App</div>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link
-                            to="/search"
-                            className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-                        > Search</Link>
+                    <li className="nav-item active">
+                        <a className="nav-link text-white" href="/">Search <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <Link
-                            to="/saved"
-                            className={window.location.pathname === "/favorites" ? "nav-link active" : "nav-link"}
-                        >Saved</Link>
+                        <a className="nav-link text-white" href="/favorites">Favorites</a>
                     </li>
                 </ul>
             </div>
