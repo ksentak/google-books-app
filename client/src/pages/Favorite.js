5e50cbe45813929ee6009175c1630930cbe39a85
KSentak <k.sentak@outlook.com>
2019-12-26 13:20:09 -0500

    Added favorite functionality

---
+++ client/src/pages/Favorite.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Results from "../components/Results";
+
+class Favorites extends Component {
+    state = {
+        favoriteBooks: [],
+    }
+
+    componentDidMount() {
+        API.savedBooks()
+            .then(favoriteBooks => this.setState({ favoriteBooks: favoriteBooks }))
+            .catch(err => console.error(err));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h2>Favorite books</h2>
+                <Results books={this.state.favoriteBooks} />
+            </div>
+        )
+    }
+}
+
+export default Favorites;
